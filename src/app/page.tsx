1cb2b48df7c3adba9a10462e20c5c7dff694b0e0
AviKnash <bakaanash@yahoo.com>
2024-09-27 19:54:36 +0530

    landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,30 @@
-import Image from "next/image";
+'use client';
+import Hero from "@/components/Hero";
 import styles from "./page.module.css";
+import { useEffect, useState } from "react";
+
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect( () => {
+    (
+      async () => {
+          const LocomotiveScroll = (await import('locomotive-scroll')).default
+          const locomotiveScroll = new LocomotiveScroll();
+
+          setTimeout( () => {
+            setIsLoading(false);
+            document.body.style.cursor = 'default'
+            window.scrollTo(0,0);
+          }, 2000)
+      }
+    )()
+  }, [])
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}></main>
-    </div>
+    <main className={styles.page}>
+      <Hero />
+    </main>
   );
 }
---
+++ src/components/Projects/index.tsx
@@ -0,0 +1,11 @@
+import styles from './style.module.scss'
+
+const Projects = () => {
+  return (
+    <div className={styles.intro}>
+      
+    </div>
+  )
+}
+
+export default Projects
